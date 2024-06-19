f16084a5202aced31e440b79138501048d96b870
«nastassiamak» <redfox.asya@gmail.com>
2024-06-19 13:28:11 +0300

    step1.js

---
+++ pyramid_generator/step1.js
@@ -0,0 +1,10 @@
+// DESCRIPTION
+
+// Variable naming follows specific rules: names can include letters, numbers, dollar signs, 
+// and underscores, but cannot contain spaces and must not begin with a number.
+// Use the let keyword to declare a variable called character.
+// Note: It is common practice to end statements in JavaScript with a semicolon. ;
+
+// SOLUTION
+
+let character;
