1bc77f5807f688eb027bccb004c4753b7608cbaf
«nastassiamak» <redfox.asya@gmail.com>
2024-06-20 12:59:21 +0300

    step1.js - 1

---
+++ GradebookApp/step1.js
@@ -0,0 +1,29 @@
+// Step 1 
+//
+// A teacher has finished grading their students' tests and needs your help 
+// to calculate the average score for the class.
+//
+// Complete the getAverage function which takes in an array of test scores 
+// and returns the average score.
+//
+//     The average is calculated by adding up all the scores and dividing by 
+//     the total number of scores.
+//  Example Code:  average = sum of all scores / total number of scores.
+// A couple of function calls have been provided for you so you can test out your code.
+//
+//     Tips
+//
+// You can use a loop to iterate over the scores array and add up all the scores.
+// You can use the length property to get the total number of scores.
+function getAverage(scores) {
+    let sum = 0;
+
+    for (const score of scores) {
+        sum += score;
+    }
+
+    return sum / scores.length;
+}
+console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
+console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
+
